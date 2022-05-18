2ab852c86a047bd57ffc72640da0a904a1a15183
Mohamed Sahel <msahelme@gmail.com>
2022-05-18 12:34:42 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      HuntVoca
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default App
